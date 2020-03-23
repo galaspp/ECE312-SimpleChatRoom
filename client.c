f5094531f6a9bbd361f554481932417bfc2bdd9c
Piotr Galas <galaspp@rose-hulman.edu>
2020-03-23 13:05:40 -0400

    Began testing

--- client.c
+++ client.c
@@ -92,7 +92,7 @@ int main(int argc, char *argv[])
     int sockfd, portno, n;
 
     struct sockaddr_in serv_addr;
-    struct hostent *server;
+    //struct hostent *server;
 
     char buffer[256];
     if (argc < 3) {
@@ -103,16 +103,14 @@ int main(int argc, char *argv[])
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
         error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
+    // server = gethostbyname(argv[1]);
+    // if (server == NULL) {
+    //     fprintf(stderr,"ERROR, no such host\n");
+    //     exit(0);
+    // }
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+   // bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
